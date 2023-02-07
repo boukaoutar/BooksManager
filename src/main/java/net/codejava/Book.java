673a7fbccffb5af72545485afd1496638e98a791
boukaoutar <boulguidkaoutar@gmail.com>
2023-02-07 16:01:15 +0100

    jpa mysql

---
+++ src/main/java/net/codejava/Book.java
@@ -0,0 +1,45 @@
+package net.codejava;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+	private Integer bookId;
+	private String title;
+	private String author;
+	private float price;
+	
+	@Id
+	@Column(name= "book_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Integer getBookId() {
+		return bookId;
+	}
+	public void setBookId(Integer bookId) {
+		this.bookId = bookId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+	
+}
